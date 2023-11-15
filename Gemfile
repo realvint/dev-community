82ff6f867fed61df6fcb1681066661edbbd0354e
Victor Malukhin <victor.malukhin@gmail.com>
2023-11-15 20:06:57 +0300

    Add rspec environment

--- Gemfile
+++ Gemfile
@@ -11,21 +11,30 @@ gem 'cssbundling-rails'
 gem 'jsbundling-rails'
 gem 'propshaft'
 
+gem 'dotenv-rails', '2.8.1', require: 'dotenv/rails-now'
 gem 'redis', '>= 4.0.1'
 gem 'stimulus-rails'
 gem 'turbo-rails'
 
 group :development, :test do
   gem 'debug', platforms: %i[mri windows]
+  gem 'factory_bot_rails', '~> 6.2'
+  gem 'faker', '~> 3.2', '>= 3.2.1'
 end
 
 group :development do
   gem 'rubocop', '~> 1.57'
+  gem 'rubocop-factory_bot', '~> 2.24'
   gem 'rubocop-rails', '~> 2.21'
+  gem 'rubocop-rspec', '~> 2.24'
   gem 'web-console'
 end
 
 group :test do
   gem 'capybara'
+  gem 'database_cleaner', '~> 2.0', '>= 2.0.2'
+  gem 'rspec-rails', '~> 6.0', '>= 6.0.3'
   gem 'selenium-webdriver'
+  gem 'shoulda-matchers', '~> 5.0'
+  gem 'webdrivers'
 end
--- config/application.rb
+++ config/application.rb
@@ -2,26 +2,30 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module DevCommunity
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.time_zone = 'Moscow'
+    config.location = 'Europe/Moscow'
+    config.i18n.default_locale = :en
+
     config.autoload_lib(ignore: %w(assets tasks))
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.assets false
+      g.test_framework :rspec, fixture: false
+      g.helper_specs false
+      g.controller_specs false
+      g.system_tests false
+      g.view_specs false
+    end
+
+    # GZip all responses
+    config.middleware.use Rack::Deflater
   end
 end
