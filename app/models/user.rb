50e7aaa3805722398bf78d503f05942db3494074
Victor Malukhin <victor.malukhin@gmail.com>
2023-11-16 13:48:27 +0300

    Display list of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ApplicationRecord
          :rememberable,
          :validatable,
          :trackable
+
+  PROFILE_TITLE = [
+    'Junior Ruby on Rails Developer',
+    'Middle Ruby on Rails Developer',
+    'Senior Ruby on Rails Developer',
+    'Full Stack Ruby on Rails Developer',
+    'Senior Java Developer',
+    'Senior Front End Developer'
+  ].freeze
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+ActiveRecord::Base.transaction do
+  1000.times do |i|
+    User.create!(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      username: "#{Faker::Name.last_name.downcase}_#{i + 10}",
+      email: Faker::Internet.email,
+      contact_number: Faker::PhoneNumber.phone_number_with_country_code,
+      street_address: Faker::Address.street_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      country: Faker::Address.country,
+      pincode: Faker::Address.postcode,
+      date_of_birth: (Time.zone.today + rand(1..30).days) - rand(24..36).years,
+      profile_title: User::PROFILE_TITLE.sample,
+      password: 'password',
+      about: Faker::Lorem.paragraphs
+    )
+    puts "User #{i + 1} created successfully"
+  end
+end
