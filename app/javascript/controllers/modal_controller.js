cb9717431fe4eed16582002bff3e157e495c9e20
Victor Malukhin <victor.malukhin@gmail.com>
2023-11-17 14:17:39 +0300

    Fix reopen modal frame

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,5 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
+document.addEventListener("turbo:before-cache", function() {
+  // remove modal since it will be opened automatically on connect
+  // we need to do that by removing parent turbo-frame id='modal' which has src
+  let modals = document.querySelectorAll('[data-controller="modal"]')
+  modals.forEach(function(modal) {
+    let parentTurboFrame = modal.closest('turbo-frame')
+    if (parentTurboFrame) {
+      parentTurboFrame.innerHTML = ''
+      parentTurboFrame.src = null
+    } else {
+      modal.remove()
+    }
+  })
+})
+
 // Connects to data-controller="modal"
 export default class extends Controller {
   connect() {
