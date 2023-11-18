9cdc854f69097f3303c37c90510efede24e1e1b0
Victor Malukhin <victor.malukhin@gmail.com>
2023-11-18 23:17:02 +0300

    Deleting work experience using turbo-stream

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -27,7 +27,13 @@ class WorkExperiencesController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    @work_experience.destroy
+
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("work-experience-item-#{@work_experience.id}") }
+    end
+  end
 
   private
 
